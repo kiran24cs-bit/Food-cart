38a62e04bdde84451595006a0ca6efe535520ac8
Kiran <kiran24cs@rnsit.ac.in>
2026-08-12 18:54:54 +0530

    some changes

--- loginsignup.js
+++ loginsignup.js
@@ -1,6 +1,8 @@
 let logindiv=document.getElementById("logindiv");
 let registerdiv=document.getElementById("registerdiv");
 registerdiv.style.display="none";
+let errorp=document.getElementById("errorshow");
+let errorpr=document.getElementById("errorshowregister");
 function loadregisterdiv(){
     logindiv.style.display="none";
     registerdiv.style.display="flex";
@@ -10,8 +12,23 @@ function loadlogindiv(){
     registerdiv.style.display="none";
 }
 function validateuser(){
-    
+    let number=document.getElementById("numberinput").value;
+    let password=document.getElementById("passwordinput").value;
+    if(!number || !password){
+        errorp.innerText="Fill all fields";
+        return;
+    }
+    errorp.innerText="";
+    console.log(number,password);
 }
 function adduser(){
+    let number=document.getElementById("numberinputregister").value;
+    let password=document.getElementById("passwordinputregister").value;
+    if(!number || !password){
+        errorpr.innerText="Fill all fields";
+        return;
+    }
+    errorpr.innerText="";
+    console.log(number,password);
 
 }
